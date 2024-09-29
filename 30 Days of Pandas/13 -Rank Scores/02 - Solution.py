44aba76bba68b222014a8e77ea3b6f1909e3e10f
Lucas Alcoléa Oue <lucasoue11@gmail.com>
2024-09-30 05:38:51 +0900

    Approach

--- 30 Days of Pandas/13 -Rank Scores/02 - Solution.py
+++ 30 Days of Pandas/13 -Rank Scores/02 - Solution.py
@@ -1,5 +1,6 @@
 '''
 Approach
-  1. Use the rank method with method ='dense' to assing ranks to the scores
-
+  1. Use the rank method with method ='dense' to assing ranks to the scores in descending order while handling ties. Add a new colum 'rank' to the scores DataFrame to store the ranks
+  2. Sort the DataFrame by score in descending order. This can be achieved using the sort_values method. 
 '''
+
