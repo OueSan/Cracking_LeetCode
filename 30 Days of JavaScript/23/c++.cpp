6edbe403dcb53d57a08b45aebac42620134ae834
Lucas Alcoléa Oue <lucasoue11@gmail.com>
2024-11-20 07:30:52 +0900

    cpp

---
+++ 30 Days of JavaScript/23/c++.cpp
@@ -0,0 +1,27 @@
+using system;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "John";
+      console.WriteLine(name);  
+     }
+  }
+
+  
+}using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "John";
+      Console.WriteLine(name);  
+     }
+  }
+}
