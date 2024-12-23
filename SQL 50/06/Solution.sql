96da0f0ad06dc4a401c42c08f944797010386c2c
Lucas Oue <121897566+OueSan@users.noreply.github.com>
2024-12-24 07:02:51 +0900

    solution

---
+++ SQL 50/06/Solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select b.unique_id,a.name #here i put "alias" for the colums i want to join as b. and a. 
+from Employees as a 
+left join EmployeeUNI as b #can be right join whatever
+on a.id = b.id #if the 2 colums are equal, they'll combine, intead will return null 
