f863969512ad1951cc2296e99e2d48b52b238b54
Lucas Oue <121897566+OueSan@users.noreply.github.com>
2024-12-24 07:03:22 +0900

    misspelling

--- SQL 50/06/Solution.sql
+++ SQL 50/06/Solution.sql
@@ -2,4 +2,4 @@
 select b.unique_id,a.name #here i put "alias" for the colums i want to join as b. and a. 
 from Employees as a 
 left join EmployeeUNI as b #can be right join whatever
-on a.id = b.id #if the 2 colums are equal, they'll combine, intead will return null 
+on a.id = b.id #if the 2 colums are equal, they'll combine, intead they'll return null 
