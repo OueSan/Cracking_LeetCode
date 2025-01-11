a0797310e5501fbbd7341f03e34c2c02e3787287
Lucas Oue <121897566+OueSan@users.noreply.github.com>
2025-01-12 07:42:39 +0900

    att leetcode out of sistem

--- SQL 50/21/Solution.sql
+++ SQL 50/21/Solution.sql
@@ -3,4 +3,4 @@ select round(((count(case when a = 'immediate' then 1 end ))/count(a))*100,2) as
 #select * from
 (select customer_id,min(order_date),min(customer_pref_delivery_date), (case when min(order_date) = min(customer_pref_delivery_date) then 'immediate' else 'scheduled' end) as a
 from Delivery
-group by 1) b
+group by 1) 
