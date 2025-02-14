17f319b6c221563f4f7754886d60081a94b0a599
Lucas` <lucasoue11@gmail.com`>
2025-02-14 23:44:29 +0900

    attatatat

--- Done/Lenguage Study/HelloWorld.java
+++ Done/Lenguage Study/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-      System.out.println("Hello, World aoba!");
+      System.out.println("Hello, World aobadssadasdasa!");
   }
 }
