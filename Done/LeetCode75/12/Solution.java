0c1bfd05f024cc91552c0eb5577536d865de01ac
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-10 11:18:10 +0900

    Doc - Solution In Java

--- Done/LeetCode75/12/Solution.java
+++ Done/LeetCode75/12/Solution.java
@@ -1,5 +1,19 @@
-package Done.LeetCode75.12;
-
-public class Solution {
-  
+class Solution {
+  static {
+      for (int i = 0; i < 500; i++) {
+          maxArea(new int[] { 0, 0 });
+      }
+  }
+  public static int maxArea(int[] height) {
+      int res = 0;
+      int left = 0;
+      int right = height.length - 1;
+      while (left < right) {
+          int min = Math.min(height[left], height[right]);
+          res = Math.max(res, min * (right - left));
+          while (left < right && height[left] <= min) left++;
+          while (left < right && height[right] <= min) right--;
+      }
+      return res;
+  }
 }
