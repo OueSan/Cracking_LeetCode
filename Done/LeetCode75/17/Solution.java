d8a428a6bb37f576a749e8b4e3d878d541ce821a
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-15 05:15:46 +0900

    Documentation

--- Done/LeetCode75/17/Solution.java
+++ Done/LeetCode75/17/Solution.java
@@ -1,23 +1,27 @@
 class Solution {
   public int longestSubarray(int[] nums) {
       int n = nums.length;
-
-      int left = 0;
-      int zeros = 0;
-      int ans = 0;
-
-      for (int right = 0; right < n; right++) {
-          if (nums[right] == 0) {
-              zeros++;
-          }
-          while (zeros > 1) {
-              if (nums[left] == 0) {
-                  zeros--;
-              }
-              left++;
-          }
-          ans = Math.max(ans, right - left + 1 - zeros);
+      int k1 = 0, k2 = 0, i = 0;
+      int mx = 0;
+      while (i < n && nums[i] != 0) {
+          k1 += 1;
+          i += 1;
       }
-      return (ans == n) ? ans - 1 : ans;
+      if (i == n) {
+          return n-1;
+      }
+      i += 1;
+      for (; i < n; i++) {
+          if (nums[i] != 0) {
+              k2 += 1;
+          }
+          else {
+              mx = Math.max(mx, k1 + k2);
+              k1 = k2;
+              k2 = 0;
+          }
+      }
+      mx = Math.max(mx, k1 + k2);
+      return mx;
   }
 }
