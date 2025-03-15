a100be98030c921de2778297f82df6edd0c64621
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-15 12:01:45 +0900

    Documentation - Java Solution

--- Done/LeetCode75/18/Solution.java
+++ Done/LeetCode75/18/Solution.java
@@ -1,5 +1,15 @@
-package Done.LeetCode75.18;
+class Solution {
+  public int largestAltitude(int[] gain) {
+      int maxAltitude = 0;
+      int result = 0;
+      for(int item : gain){
+          result += item;
+          if(result > maxAltitude){
+              maxAltitude = result;
+          }
+      }
 
-public class Solution {
-  
+      return maxAltitude;
+      
+  }
 }
