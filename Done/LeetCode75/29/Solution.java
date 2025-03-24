a372fd74218a7d16a3e87a5b3ca7afc8deb6a908
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-25 07:18:22 +0900

    Documentation - Solution

--- Done/LeetCode75/29/Solution.java
+++ Done/LeetCode75/29/Solution.java
@@ -1,5 +1,28 @@
-package Done.LeetCode75.29;
+class Solution {
+  public String predictPartyVictory(String s ) {
+  return recursion(s.toCharArray(), s.length(), 0);
+}
 
-public class Solution {
+private static String recursion(char[] s, int leng, int state) {
+  int newLeng = 0;
+  for( int i = 0 ; i < leng; i++) {
+    if(s[i] == 'R') {
+      if(state >= 0) {
+        s[newLeng++] = 'R';
+      }
+      state++;
+    }
+    else {
+      if(state <= 0) {
+        s[newLeng++] = 'D';
+      }
+      state--;
+    }
+  }
+  if(Math.abs(state) >= newLeng) {
+    return (state >= 0 ) ? "Radiant" : "Dire";
+  }
   
+  return recursion(s, newLeng, state);
+}
 }
