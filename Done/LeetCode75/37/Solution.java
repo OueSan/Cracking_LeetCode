75485b7ad0b756ff3985da2a04bc5ecf6a826cbd
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-28 04:35:19 +0900

    Documentation - Solution

--- Done/LeetCode75/37/Solution.java
+++ Done/LeetCode75/37/Solution.java
@@ -1,5 +1,18 @@
-package Done.LeetCode75.37;
+class Solution {
+  int total = 0;
+  public int pathSum(TreeNode root, int sum) {
+      if(root == null) return 0;
+      helper(root, sum, 0);
+      pathSum(root.left, sum);
+      pathSum(root.right, sum);
+      return total;
+  }
 
-public class Solution {
-  
+  void helper(TreeNode root, int sum, long curr) {
+      if(root == null) return;
+      curr += root.val;
+      if(curr == sum) total++;
+      helper(root.left, sum, curr);
+      helper(root.right, sum, curr);
+  }
 }
