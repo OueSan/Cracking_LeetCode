3e60624d174c07538c22cfadf5651eed7c66df32
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-27 21:09:45 +0900

    Documentation - Solution

--- Done/LeetCode75/34/Solution.java
+++ Done/LeetCode75/34/Solution.java
@@ -1,5 +1,8 @@
-package Done.LeetCode75.34;
-
-public class Solution {
-  
+class Solution {
+  public int maxDepth(TreeNode root) {
+      if (root == null) {
+          return 0;
+      }
+      return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));        
+  }
 }
