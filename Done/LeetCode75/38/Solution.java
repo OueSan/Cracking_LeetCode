643744e956ed41abddf00ebdbb24952cb4b619dc
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-29 06:34:24 +0900

    Documentation - Solution

--- Done/LeetCode75/38/Solution.java
+++ Done/LeetCode75/38/Solution.java
@@ -1,5 +1,21 @@
-package Done.LeetCode75.38;
+class Solution {
+  public int maxLength=0;
+  public void solve(TreeNode root,int dir,int currLength){
+      if(root==null) return;
+      maxLength=Math.max(maxLength,currLength);
+      if(dir==1){
+          solve(root.left,0,currLength+1);
+          solve(root.right,1,1);
+      }
+      else{
+          solve(root.right,1,currLength+1);
+          solve(root.left,0,1);
+      }
+  }
 
-public class Solution {
-  
+  public int longestZigZag(TreeNode root) {
+      solve(root,0,0);
+      solve(root,1,0);
+      return maxLength;
+  }
 }
