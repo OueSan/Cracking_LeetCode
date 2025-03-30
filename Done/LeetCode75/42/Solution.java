590a1d97c028973b181def57b561318ecc937d4d
OueSan <121897566+OueSan@users.noreply.github.com>
2025-03-31 06:45:13 +0900

    Documentation - Solution

--- Done/LeetCode75/42/Solution.java
+++ Done/LeetCode75/42/Solution.java
@@ -1,5 +1,14 @@
-package Done.LeetCode75.42;
-
-public class Solution {
-  
+class Solution {
+  public TreeNode searchBST(TreeNode root, int val) {
+      if (root == null) {
+          return null;
+      }
+      if (root.val == val) {
+          return root;
+      } else if (root.val > val) {
+          return searchBST(root.left, val);
+      } else {
+          return searchBST(root.right, val);
+      }        
+  }
 }
