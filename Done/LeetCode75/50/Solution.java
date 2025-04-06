b0e1f0cb2807608694ba86cb26dcdcb6a841e1d8
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-07 05:21:23 +0900

    Documentation - Solution

--- Done/LeetCode75/50/Solution.java
+++ Done/LeetCode75/50/Solution.java
@@ -1,5 +1,16 @@
-package Done.LeetCode75.50;
-
-public class Solution {
-  
+class Solution {
+  public int findKthLargest(int[] nums, int k) {
+     int n = nums.length;
+     if(n == 0) return -1;
+     if(n<k) return -1;
+     int [] count = new int[20001];
+     for(int i:nums){
+         count[i+10000]++;
+     }
+     for(int i = 20000;i>0;i--){
+      if(count[i]>0) k -= count[i];
+      if(k<=0) return i - 10000;
+     }
+     return -1;
+  }
 }
