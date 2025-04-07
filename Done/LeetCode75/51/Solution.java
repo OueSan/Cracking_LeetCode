7d96cd013822fbf5c6d9eeb28dd3020c755f8d10
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-08 08:46:33 +0900

    Documentation - Solution

--- Done/LeetCode75/51/Solution.java
+++ Done/LeetCode75/51/Solution.java
@@ -1,5 +1,27 @@
-package Done.LeetCode75.51;
+class SmallestInfiniteSet {
+  private int current;
+  private PriorityQueue<Integer> minHeap;
+  private Set<Integer> addedBack;
 
-public class Solution {
-  
+  public SmallestInfiniteSet() {
+      current = 1;
+      minHeap = new PriorityQueue<>();
+      addedBack = new HashSet<>();
+  }
+
+  public int popSmallest() {
+      if (!minHeap.isEmpty()) {
+          int smallest = minHeap.poll();
+          addedBack.remove(smallest);
+          return smallest;
+      }
+      return current++;
+  }
+
+  public void addBack(int num) {
+      if (num < current && !addedBack.contains(num)) {
+          minHeap.offer(num);
+          addedBack.add(num);
+      }
+  }
 }
