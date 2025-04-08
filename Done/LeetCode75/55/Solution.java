cabb279f799dc2e276c64c5a04c920e1ba51b28b
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-09 07:39:29 +0900

    Documentation - Solution

--- Done/LeetCode75/55/Solution.java
+++ Done/LeetCode75/55/Solution.java
@@ -1,5 +1,17 @@
-package Done.LeetCode75.55;
+class Solution {
+  public int findPeakElement(int[] nums) {
+      int left = 0;
+      int right = nums.length - 1;
 
-public class Solution {
-  
+      while (left < right) {
+          int mid = (left + right) / 2;
+          if (nums[mid] > nums[mid + 1]) {
+              right = mid;
+          } else {
+              left = mid + 1;
+          }
+      }
+
+      return left;        
+  }
 }
