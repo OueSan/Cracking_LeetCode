85157c769f085b27341b69fe9121a0caa91bd6e0
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-15 05:07:46 +0900

    Documentation - Solution

--- Done/LeetCode75/68/Solution.java
+++ Done/LeetCode75/68/Solution.java
@@ -1,5 +1,16 @@
-package Done.LeetCode75.68;
+class Solution {
+  public int[] countBits(int n) {
+      int[] dp = new int[n + 1];
+      int sub = 1;
 
-public class Solution {
-  
+      for (int i = 1; i <= n; i++) {
+          if (sub * 2 == i) {
+              sub = i;
+          }
+
+          dp[i] = dp[i - sub] + 1;
+      }
+
+      return dp;        
+  }
 }
