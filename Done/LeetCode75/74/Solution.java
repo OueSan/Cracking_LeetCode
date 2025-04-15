60cc5c7172343bbefcf1b587e58c50ab555ea99b
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-16 05:07:18 +0900

    Documentation - Solution

--- Done/LeetCode75/74/Solution.java
+++ Done/LeetCode75/74/Solution.java
@@ -1,5 +1,14 @@
-package Done.LeetCode75.74;
+class Solution {
+  public int findMinArrowShots(int[][] points) {
+      Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+      int count = 1, end = points[0][1];
 
-public class Solution {
-  
+      for(int i = 1; i < points.length; i++) {
+          if(points[i][0] > end){
+              count++;
+              end = points[i][1];
+          }
+      }
+      return count;
+  }
 }
