28e9a9461bb1fa187ddaebda2c01daf938c58b2a
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-19 07:53:59 +0900

    Documentation - Solution

--- Done/Binary_Serach/03/Solution.java
+++ Done/Binary_Serach/03/Solution.java
@@ -1,5 +1,23 @@
-package Done.Binary_Serach.03;
+class Solution {
+  public char nextGreatestLetter(char[] letters, char target) {
+      int low = 0;
+      int high = letters.length - 1;
 
-public class Solution {
-  
+      while (low <= high) {
+          int middle = (low + high) / 2;
+          char guess = letters[middle];
+
+          if (guess <= target) {
+              low = middle + 1;
+          } else {
+              high = middle - 1;
+          }
+      }
+
+      if (low < letters.length) {
+          return letters[low];
+      } else {
+          return letters[0];
+      }
+  }
 }
