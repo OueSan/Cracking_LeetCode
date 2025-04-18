fd831887bb203b5dbbce364909a14f9c83b8e8bd
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-19 06:49:14 +0900

    Documentation

--- Done/Binary_Serach/02/Solution.java
+++ Done/Binary_Serach/02/Solution.java
@@ -1,5 +1,23 @@
-package Done.Binary_Serach.02;
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+      int low = 0;
+      int high = nums.length - 1;
 
-public class Solution {
-  
+      while (low <= high) {
+          int middle = (low + high) / 2;
+          int guess = nums[middle];
+
+          if (guess == target) {
+              return middle;
+          }
+
+          if (guess > target) {
+              high = middle - 1;
+          } else {
+              low = middle + 1;
+          }
+      }
+
+      return low;
+  }
 }
