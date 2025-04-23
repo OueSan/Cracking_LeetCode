b5efa5df32f615454d0cc173853d9017029c48be
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-23 20:39:38 +0900

    Documentation

--- Done/Binary_Serach/09/Solution.java
+++ Done/Binary_Serach/09/Solution.java
@@ -1,5 +1,28 @@
-package Done.Binary_Serach.09;
+class Solution {
+  public int search(int[] nums, int target) {
+      int left = 0;
+      int right = nums.length - 1;
 
-public class Solution {
-  
+      while (left <= right) {
+          int mid = (left + right) / 2;
+
+          if (nums[mid] == target) {
+              return mid;
+          } else if (nums[mid] >= nums[left]) {
+              if (nums[left] <= target && target <= nums[mid]) {
+                  right = mid - 1;
+              } else {
+                  left = mid + 1;
+              }
+          } else {
+              if (nums[mid] <= target && target <= nums[right]) {
+                  left = mid + 1;
+              } else {
+                  right = mid - 1;
+              }
+          }
+      }
+
+      return -1;        
+  }
 }
