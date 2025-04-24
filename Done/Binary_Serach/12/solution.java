cf6bd8343ea262a7e1aafd51093523828888a0c2
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-24 09:39:12 +0900

    Documentation

--- Done/Binary_Serach/12/solution.java
+++ Done/Binary_Serach/12/solution.java
@@ -1,5 +1,20 @@
-package Done.Binary_Serach.12;
+public class Solution extends GuessGame {
+  public int guessNumber(int n) {
+      int low = 0;
+      int high = n;
+      int mid = 0;
 
-public class solution {
-  
+      while (low <= high) {
+          mid = low + (high - low) / 2;
+
+          if (guess(mid) == 0)
+              return mid;
+          if (guess(mid) == -1)
+              high = mid - 1;
+          if (guess(mid) == 1)
+              low = mid + 1;
+      }
+
+      return -1;
+  }
 }
