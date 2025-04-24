b60d7fceb78f2573dad5333882e2c6d4579262a7
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-24 09:34:50 +0900

    Documentation

--- Done/Binary_Serach/11/solution.java
+++ Done/Binary_Serach/11/solution.java
@@ -1,5 +1,21 @@
-package Done.Binary_Serach.11;
-
-public class solution {
-  
+class Solution {
+  public int findMin(int[] nums) {
+      int lo = 0, hi = nums.length - 1;
+      
+      if (nums[lo] < nums[hi]) return nums[lo];
+      
+      while (lo < hi) {
+          int mid = lo + (hi - lo) / 2;
+          
+          if (nums[mid] > nums[hi]) {
+              lo = mid + 1;
+          } else if (nums[mid] < nums[hi]) {
+              hi = mid;
+          } else {
+              hi--;
+          }
+      }
+      
+      return nums[hi];
+  }
 }
