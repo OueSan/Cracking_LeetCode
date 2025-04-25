51d1928c7178b509b679ccc86bb07261607ce116
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-25 21:19:07 +0900

    Documentation

--- Done/Binary_Serach/13/Solution.java
+++ Done/Binary_Serach/13/Solution.java
@@ -1,5 +1,11 @@
-package Done.Binary_Serach.13;
-
-public class Solution {
-  
+public class Solution extends VersionControl {
+  public int firstBadVersion(int n) {
+      int l=0,h=n;
+      while(l<=h){
+          int mid = l+(h-l)/2;
+          if(isBadVersion(mid)) h = mid-1;
+          else l = mid+1;
+      }
+      return l;
+  }
 }
