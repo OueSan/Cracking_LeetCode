b415f5896e370d333692a4dbb54e6d98d56c2df1
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-25 21:20:40 +0900

    Documentation

--- Done/Binary_Serach/13/Solution.java
+++ Done/Binary_Serach/13/Solution.java
@@ -1,11 +1,19 @@
-public class Solution extends VersionControl {
-  public int firstBadVersion(int n) {
-      int l=0,h=n;
-      while(l<=h){
-          int mid = l+(h-l)/2;
-          if(isBadVersion(mid)) h = mid-1;
-          else l = mid+1;
-      }
-      return l;
-  }
-}
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int low = 0;
+            int high = n;
+            while(low<=high){
+                int mid = low+(high-low)/2;
+                if(isBadVersion(mid)){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            return low;
+        }
+    }
