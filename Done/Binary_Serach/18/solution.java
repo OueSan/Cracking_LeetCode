fcfaf4aa3b2893f8833bc7c49ee33d2568a14d36
OueSan <121897566+OueSan@users.noreply.github.com>
2025-04-28 17:25:45 +0900

    Documentation

--- Done/Binary_Serach/18/solution.java
+++ Done/Binary_Serach/18/solution.java
@@ -1,5 +1,14 @@
-package Done.Binary_Serach.18;
-
-public class solution {
-  
+class Solution {
+  public int findKthPositive(int[] arr, int k) {
+      int left = 0, right = arr.length - 1;
+      while (left <= right) {
+          int mid = left + (right - left) / 2;
+          if (arr[mid] - (mid + 1) >= k) {
+              right = mid - 1;
+          } else {
+              left = mid + 1;
+          }
+      }
+      return left + k;
+  }
 }
