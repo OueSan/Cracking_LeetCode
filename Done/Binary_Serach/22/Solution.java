b5cdb5a68057c3751d661b9c406061f7601ea92d
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-01 08:20:56 +0900

    Documentation

--- Done/Binary_Serach/22/Solution.java
+++ Done/Binary_Serach/22/Solution.java
@@ -1,5 +1,18 @@
-package Done.Binary_Serach.22;
-
-public class Solution {
-  
+class Solution {
+  public List<Integer> findClosestElements(int[] arr, int k, int x) {
+      int lo = 0;
+  int hi = arr.length - 1;
+  while (hi - lo >= k) {
+    if (Math.abs(arr[lo] - x) > Math.abs(arr[hi] - x)) {
+      lo++;
+    } else {
+      hi--;
+    }
+  }
+  List<Integer> result = new ArrayList<>(k);
+  for (int i = lo; i <= hi; i++) {
+    result.add(arr[i]);
+  }
+  return result;
+  }
 }
