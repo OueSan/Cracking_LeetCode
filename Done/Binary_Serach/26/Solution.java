a945dca935a8e9b50aea09718b3cbfcf624980ec
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-03 07:07:42 +0900

    Documentation

--- Done/Binary_Serach/26/Solution.java
+++ Done/Binary_Serach/26/Solution.java
@@ -1,5 +1,26 @@
-package Done.Binary_Serach.26;
-
-public class Solution {
-  
+class Solution {
+  public int[] successfulPairs(int[] spells, int[] potions, long success) {
+      int n = spells.length;
+      int m = potions.length;
+      int[] res = new int[n];
+      int max = 0;
+      for (int i=0;i<m;i++){
+          max = Math.max(max, potions[i]);
+      }
+      int[] map = new int[max+1];
+      for (int i=0;i<m;i++){map[potions[i]]++;}
+      int sum=0;
+      for (int i=max;i>=0;i--){
+          sum += map[i];
+          map[i] = sum;
+      }
+      for (int i=0;i<n;i++){
+          int spell = spells[i];
+          long index = (success+spell-1)/spell;
+          if (index<=max){
+              res[i]=map[(int) index];
+          }
+      }
+  return res;
+  }
 }
