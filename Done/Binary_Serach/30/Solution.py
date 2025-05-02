81244880110d3a3980e200cec017c821bda518f5
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-03 07:50:36 +0900

    Documentation

--- Done/Binary_Serach/30/Solution.py
+++ Done/Binary_Serach/30/Solution.py
@@ -1,18 +1,22 @@
+class Solution:
     def maxEnvelopes(self, envelopes: List[List[int]]) -> int:
         envelopes.sort(key=lambda x: (x[0], -x[1]))
-        LIS = []
-        size = 0
-        for (w, h) in envelopes:
-            if not LIS or h > LIS[-1]:
-                LIS.append(h)
-                size += 1
+        
+        res = []		
+		# Perform LIS
+        for _, h in envelopes:
+            l,r=0,len(res)-1
+			# find the insertion point in the Sort order
+            while l <= r:
+                mid=(l+r)>>1
+                if res[mid]>=h:
+                    r=mid-1
+                else:
+                    l=mid+1        
+            idx = l
+            if idx == len(res):
+                res.append(h)
             else:
-                l, r = 0, size
-                while l < r:
-                    m = l + (r - l) // 2
-                    if LIS[m] < h:
-                        l = m + 1
-                    else:
-                        r = m
-                LIS[l] = h
-        return size
+                res[idx]=h
+        return len(res)
+                                               
