c41192faf8c0ae34836c10cfbf3302f5bdd25510
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-03 07:10:15 +0900

    Documentation

--- Done/Binary_Serach/27/Solution.java
+++ Done/Binary_Serach/27/Solution.java
@@ -1,5 +1,22 @@
-package Done.Binary_Serach.27;
+class Solution {
+  public int numSubseq(int[] nums, int target) {
+      int res = 0, mod = 1000000007, l = 0, r = nums.length - 1;
+      List<Integer> pre = new ArrayList<>();
+      pre.add(1);
+      for (int i = 1; i <= nums.length; ++i) {
+          pre.add((pre.get(i - 1) << 1) % mod);
+      }
 
-public class Solution {
-  
+      Arrays.sort(nums);
+
+      while (l <= r) {
+          if (nums[l] + nums[r] > target) {
+              r--;
+          } else {
+              res = (res + pre.get(r - l++)) % mod;
+          }
+      }
+
+      return res;
+  }
 }
