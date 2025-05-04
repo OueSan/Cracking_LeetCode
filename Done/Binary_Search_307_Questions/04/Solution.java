df236d903229dc0834d4bfb1e838fdcde0a64292
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-05 06:47:54 +0900

    Documentation

---
+++ Done/Binary_Search_307_Questions/04/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+  public int searchInsert(int[] nums, int target) {
+      int low = 0;
+      int high = nums.length - 1;
+
+      while (low <= high) {
+          int middle = (low + high) / 2;
+          int guess = nums[middle];
+
+          if (guess == target) {
+              return middle;
+          }
+
+          if (guess > target) {
+              high = middle - 1;
+          } else {
+              low = middle + 1;
+          }
+      }
+
+      return low;
+  }
+}
