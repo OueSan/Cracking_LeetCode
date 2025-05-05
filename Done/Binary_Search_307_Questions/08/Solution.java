943ff2c2475870fb2c83a2609eca90cf0f9f8e57
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-06 08:08:16 +0900

    Documentation

--- Done/Binary_Search_307_Questions/08/Solution.java
+++ Done/Binary_Search_307_Questions/08/Solution.java
@@ -1,5 +1,15 @@
-package Done.Binary_Search_307_Questions.08;
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+    
+        left = 0
+        right = len(nums) - 1
 
-public class Solution {
-  
-}
+        while left < right:
+            mid = (left + right) // 2
+
+            if nums[mid] <= nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+        
+        return nums[left]
