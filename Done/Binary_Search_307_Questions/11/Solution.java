770ce1377cbe88bac436e598339c239cbcc9fc1f
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-07 06:41:29 +0900

    Documentation

--- Done/Binary_Search_307_Questions/11/Solution.java
+++ Done/Binary_Search_307_Questions/11/Solution.java
@@ -1,5 +1,19 @@
-package Done.Binary_Search_307_Questions.11;
+class Solution {
+  public int[] twoSum(int[] numbers, int target) {
+      int left = 0;
+      int right = numbers.length - 1;
 
-public class Solution {
-  
+      while (left < right) {
+          int total = numbers[left] + numbers[right];
+
+          if (total == target) {
+              return new int[]{left + 1, right + 1};
+          } else if (total > target) {
+              right--;
+          } else {
+              left++;
+          }
+      }
+      return new int[]{-1, -1}; // If no solution is found        
+  }
 }
