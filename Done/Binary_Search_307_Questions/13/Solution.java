263f5846aa8f65d6efd4835a8c44a3cbdddccca6
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-08 07:39:58 +0900

    Documentation

--- Done/Binary_Search_307_Questions/13/Solution.java
+++ Done/Binary_Search_307_Questions/13/Solution.java
@@ -1,5 +1,11 @@
-package Done.Binary_Search_307_Questions.13;
-
-public class Solution {
-  
+class Solution {
+  int height(TreeNode root) {
+      return root == null ? -1 : 1 + height(root.left);
+  }
+  public int countNodes(TreeNode root) {
+      int h = height(root);
+      return h < 0 ? 0 :
+             height(root.right) == h-1 ? (1 << h) + countNodes(root.right)
+                                       : (1 << h-1) + countNodes(root.left);
+  }
 }
