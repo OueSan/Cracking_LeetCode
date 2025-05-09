586fa7c9c1c57d8cfd688137b039068ffaab99f9
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-09 09:18:18 +0900

    Documentation

--- Done/Binary_Search_307_Questions/17/Solution.java
+++ Done/Binary_Search_307_Questions/17/Solution.java
@@ -1,5 +1,24 @@
-package Done.Binary_Search_307_Questions.17;
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
 
-public class Solution {
+class Solution {
+  public:
+      int firstBadVersion(int n) {
+          int first = 1;
+          int last = n;
   
-}
+          while (first < last) {
+              int mid = first + (last - first) / 2;
+  
+              if (isBadVersion(mid)) {
+                  last = mid; // Mid could be the first bad version, so narrow the
+                              // range to the left half.
+              } else {
+                  first = mid + 1; // If mid is not bad, the first bad version
+                                   // must be after mid.
+              }
+          }
+  
+          return first; // At the end, first will be the first bad version.
+      }
+  };
