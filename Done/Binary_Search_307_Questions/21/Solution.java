3c38024145faf6658be6d501138dfd156e545afc
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-11 06:32:22 +0900

    Documentation

--- Done/Binary_Search_307_Questions/21/Solution.java
+++ Done/Binary_Search_307_Questions/21/Solution.java
@@ -1,21 +1,30 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j] && !result.contains(nums1[i])){
-                    result.add(nums1[i]);
-                    continue;
-                }
+        int[] counter = new int[1001];
+        for(int num : nums1)
+        {
+            counter[num] = 1;
+        }
+        int count=0;
+        for(int num : nums2)
+        {
+            if(counter[num] == 1)
+            {
+                counter[num] = 2;
+                count++;
             }
         }
-        int [] arr=new int[result.size()];
-        int k=0;
-        for(int num:result){
-            arr[k]=num;
-            k++;
+        int[] res = new int[count];
+        int ind=0;
+        for(int i : nums2)
+        {
+            if(counter[i] == 2)
+            {
+                res[ind++] = i;
+                counter[i] = 1;
+            }
         }
-        return arr;
+        return res;
+        
     }
 }
