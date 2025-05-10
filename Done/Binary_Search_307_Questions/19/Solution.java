7d13ddbb7228e59db64d327d403a95c4f99d7df8
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-11 06:23:39 +0900

    Documentation

--- Done/Binary_Search_307_Questions/19/Solution.java
+++ Done/Binary_Search_307_Questions/19/Solution.java
@@ -1,27 +1,33 @@
-class Solution {
-    public static int find(List<Integer> arr, int x){
-        int s = 0;
-        int e = arr.size() - 1;
-        while(s <= e){
-            int m = s + (e - s)/2;
-            if(arr.get(m) >= x){
-                e = m-1;
-            }else{
-                s = m+1;
+class Solution { 
+    static{
+        for(int i = 0 ; i < 500 ; i++){
+            countSmaller(new int[]{1 , 1});
+        }
+    }   
+    public static List<Integer> countSmaller(int[] nums) {
+        int min = 20001;
+        int max = -1;
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+        min--;
+        int[] count = new int[max-min+1];
+        Integer[] result = new Integer[nums.length];
+        for (int i = nums.length-1; i >=0; i--) {
+            int k = nums[i]-min-1;
+            int c = 0;
+            do {
+                c += count[k];
+                k -= (-k&k);
+            } while (k > 0);
+            result[i] = c;
+            k = nums[i]-min;
+            while (k < count.length) {
+                count[k]++;
+                k += (-k & k);
             }
         }
-        return s;
-    }
-    public List<Integer> countSmaller(int[] nums) {
-        List<Integer> li = new ArrayList<>();
-        List<Integer> arr = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        
-        Collections.sort(arr);
-        for(int x : nums){
-            int i = find(arr, x);
-            li.add(i);
-            arr.remove(i);
-        }
-        return li;
+        return Arrays.asList(result);
     }
 }
