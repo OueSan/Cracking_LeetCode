fff54805745e068519c96d5341619ca52d1b7560
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-14 13:17:36 +0900

    Documentation

--- Done/Binary_Search_307_Questions/31/Solution.java
+++ Done/Binary_Search_307_Questions/31/Solution.java
@@ -1,5 +1,16 @@
-package Done.Binary_Search_307_Questions.31;
+class Solution {
+    public int findNthDigit(int n) {
+        long digitInNum = 1, start = 1, end = 9;
 
-public class Solution {
-  
+        while (n > digitInNum * end) {
+            n -= digitInNum * end;
+            digitInNum++;
+            start *= 10;
+            end *= 10;
+        }
+
+        long num = start + (n - 1) / digitInNum;
+        String numStr = Long.toString(num);
+        return numStr.charAt((int) ((n - 1) % digitInNum)) - '0';
+    }
 }
