e91aa266f2dbd391519871ab697e04bf466a65e8
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-15 06:38:10 +0900

    Documentation

--- Done/Binary_Search_307_Questions/34/Solution.java
+++ Done/Binary_Search_307_Questions/34/Solution.java
@@ -1,9 +1,5 @@
 class Solution {
     public int arrangeCoins(int n) {
-        for (long i = 1; ; i++) {
-            long sum = (i * (i + 1)) / 2;
-            if (sum > n) return (int)(i - 1);
-            if (sum == n) return (int)i;
-        }
+        return (int)((Math.sqrt(1 + 8L * n) - 1) / 2);
     }
 }
