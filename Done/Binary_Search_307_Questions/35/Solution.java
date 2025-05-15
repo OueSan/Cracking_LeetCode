a50661fe30b65769184492735c7bec02d454396d
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-16 08:58:16 +0900

    Documentation

--- Done/Binary_Search_307_Questions/35/Solution.java
+++ Done/Binary_Search_307_Questions/35/Solution.java
@@ -1,5 +1,22 @@
-package Done.Binary_Search_307_Questions.34;
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<int[]> st = new Stack<>();
+        int cur_min = nums[0];
 
-public class Solution {
-  
+        for (int i = 1; i < nums.length; i++) {
+            int n = nums[i];
+            while (!st.isEmpty() && n >= st.peek()[0]) {
+                st.pop();
+        }
+
+            if (!st.isEmpty() && n > st.peek()[1]) {
+                return true;
+            }
+
+            st.push(new int[] { n, cur_min });
+            cur_min = Math.min(cur_min, n);
+        }
+
+        return false;        
+    }
 }
