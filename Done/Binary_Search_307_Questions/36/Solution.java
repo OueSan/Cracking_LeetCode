5f50c505f6b665dc539e8ceec01232c25313187c
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-17 10:21:23 +0900

    Documentation

--- Done/Binary_Search_307_Questions/36/Solution.java
+++ Done/Binary_Search_307_Questions/36/Solution.java
@@ -1,5 +1,24 @@
-package Done.Binary_Search_307_Questions.34;
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int n = houses.length;
+        int m = heaters.length;
+        int r = Math.max(heaters[0] - houses[0], houses[n-1] - heaters[m-1]);
+        int j = 0;
 
-public class Solution {
-  
+        for (int i = 1; i < m; i++) {
+            if (heaters[i-1]+r >= heaters[i]-r) {
+                continue;
+            }
+            while (j < n && houses[j] <= heaters[i-1]+r) j++;
+
+            while (j < n && houses[j] > heaters[i-1]+r && houses[j] < heaters[i]-r) {
+                r += Math.min(houses[j] - (heaters[i-1]+r), (heaters[i]-r) - houses[j]);
+                j++;
+            }
+        }
+
+        return r;
+    }
 }
