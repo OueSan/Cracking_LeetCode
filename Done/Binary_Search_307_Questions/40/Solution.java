00c9d2ea6ad1443cb5e1ef06c782b44284cf3f3c
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-19 07:54:39 +0900

    Documentation

--- Done/Binary_Search_307_Questions/40/Solution.java
+++ Done/Binary_Search_307_Questions/40/Solution.java
@@ -1,5 +1,35 @@
-package Done.Binary_Search_307_Questions.40;
+class Solution {
+    private List<Integer> runningSums;
+    private int totalSum;
 
-public class Solution {
-  
+    public Solution(int[] weights) {
+
+        runningSums = new ArrayList<>();
+        int runningSum = 0;
+
+        for (int w : weights) {
+            runningSum += w;
+            runningSums.add(runningSum);
+        }
+
+        totalSum = runningSum;
+    }
+
+    public int pickIndex() {
+        Random random = new Random();
+        int target = random.nextInt(totalSum) + 1;
+        int low = 0;
+        int high = runningSums.size();
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (target > runningSums.get(mid)) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+
+        return low;
+    }
 }
