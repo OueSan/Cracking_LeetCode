b0da8ec14e58e4130d642cd2804b02fbfbd70dfa
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-19 08:10:20 +0900

    Documentation

--- Done/Binary_Search_307_Questions/41/Solution.java
+++ Done/Binary_Search_307_Questions/41/Solution.java
@@ -1,5 +1,15 @@
-package Done.Binary_Search_307_Questions.40;
+ // O(n) Time Solution
 
-public class Solution {
-  
-}
+ class Solution {
+ 		public int findPairs(int[] nums, int k) {
+ 			Map<Integer, Integer> map = new HashMap();
+ 			for (int num : nums)
+ 				map.put(num, map.getOrDefault(num, 0) + 1);
+
+ 			int result = 0;
+ 			for (int i : map.keySet())
+ 				if (k > 0 && map.containsKey(i + k) || k == 0 && map.get(i) > 1)
+ 					result++;
+ 			return result;
+ 		}
+ 	}
