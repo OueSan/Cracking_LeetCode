83185885416d84b987341aaa77438085ba8f6ec9
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-20 07:33:56 +0900

    Documentation

--- Done/Binary_Search_307_Questions/42/Solution.java
+++ Done/Binary_Search_307_Questions/42/Solution.java
@@ -1,5 +1,17 @@
-package Done.Binary_Search_307_Questions.40;
-
-public class Solution {
-  
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (mid % 2 == 1) {
+                mid--;
+            }
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
 }
