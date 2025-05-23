81ecc3bc5e0a014244059794bdbbfd73b94f78e9
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-24 08:40:05 +0900

    Documentation

--- Done/Binary_Search_307_Questions/49/Solution.java
+++ Done/Binary_Search_307_Questions/49/Solution.java
@@ -1,5 +1,17 @@
-package Done.Binary_Search_307_Questions.49;
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) return 0;
 
-public class Solution {
-  
+        int left = 0, right = 0, product = 1, count = 0;
+        int n = nums.length;
+
+        while (right < n) {
+            product *= nums[right];
+            while (product >= k) product /= nums[left++];
+            count += 1 + (right - left);
+            right++;
+        }
+
+        return count;
+    }
 }
