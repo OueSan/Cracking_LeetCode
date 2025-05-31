3dbfb657b47e4e2262f31dc05bbce06513cc38a5
OueSan <121897566+OueSan@users.noreply.github.com>
2025-05-31 15:58:24 +0900

    Documentation

--- Done/Binary_Search_307_Questions/55/Solution.java
+++ Done/Binary_Search_307_Questions/55/Solution.java
@@ -1,5 +1,28 @@
-package Done.Binary_Search_307_Questions.55;
+class Solution {
+    public int reachNumber(int target) {
+        target=Math.abs(target);
+        int pos=0,step=0,st=1,end=target;
 
-public class Solution {
-  
+        long sum=0;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            sum=(long)mid*(mid+1)/2;
+
+            if(sum>=target){
+                pos=(int)sum;
+                step=mid;
+                end=mid-1;
+            }
+            else{
+                st=mid+1;
+            }
+        }
+
+        while((pos-target)%2!=0){
+            step++;
+            pos+=step;
+        }
+
+        return step;
+    }
 }
