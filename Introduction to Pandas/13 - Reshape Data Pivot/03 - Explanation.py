5f10429c5df0f459b773c64854a10c932b6b62d3
Lucas Alcolea Oue <lucasoue11@gmail.com>
2025-07-18 09:11:53 +0900

    Documentation

--- Introduction to Pandas/13 - Reshape Data Pivot/03 - Explanation.py
+++
@@ -1,6 +0,0 @@
-Explanation:
-df.pivot(index='month', columns='city', values='temperature'): This creates a pivot table where:
-The month is used as the index.
-The city names become columns.
-The temperatures are the values in the table.
-This approach is both efficient and straightforward. Well done!
