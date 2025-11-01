0b80dc288526cf8182238f3bd51f160d87fdfba7
Lucas Alcolea Oue <121897566+OueSan@users.noreply.github.com>
2025-11-02 08:56:50 +0900

    Documentation

---
+++ Done/Binary_Search_307_Questions/99/Solution.py
@@ -0,0 +1,9 @@
+def findKthPositive(arr, k):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] - (mid + 1) >= k:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left + k
