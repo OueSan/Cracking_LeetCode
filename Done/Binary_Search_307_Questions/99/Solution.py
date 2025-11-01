ad71fc921d2809775238666e1d5ef99387530981
Lucas Alcolea Oue <121897566+OueSan@users.noreply.github.com>
2025-11-02 08:58:26 +0900

    Documentation

--- Done/Binary_Search_307_Questions/99/Solution.py
+++ Done/Binary_Search_307_Questions/99/Solution.py
@@ -1,9 +1,13 @@
-def findKthPositive(arr, k):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] - (mid + 1) >= k:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return left + k
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        l = 0
+        h = len(arr)
+        while h > l:
+            mid = (h+l)//2
+            miss = arr[mid]-mid-1
+            if k <= miss:
+                h = mid
+            else:
+                 l = mid+1
+        
+        return k + l
