b8fe412295858400c602eb80795a99bac8b1fd77
Lucas Alcolea Oue <121897566+OueSan@users.noreply.github.com>
2025-11-02 09:02:16 +0900

    Documentation

--- Done/Binary_Search_307_Questions/101/Solution.java
+++ Done/Binary_Search_307_Questions/101/Solution.java
@@ -1,14 +1,36 @@
-    public int findLatestStep(int[] A, int m) {
-        int res = -1, n = A.length;
-        int[] length = new int[n + 2], count = new int[n + 1];
+import java.util.*;
+
+class Solution {
+    public int findLatestStep(int[] arr, int m) {
+        int n = arr.length;
+        int ans = -1;
+        
+        // Arrays to keep track of lengths of segments ending at each position and counts of these lengths
+        int[] lenAtIndex = new int[n + 2];
+        int[] countLen = new int[n + 2];
+
         for (int i = 0; i < n; ++i) {
-            int a = A[i], left = length[a - 1], right = length[a + 1];
-            length[a] = length[a - left] = length[a + right] = left + right + 1;
-            count[left]--;
-            count[right]--;
-            count[length[a]]++;
-            if (count[m] > 0)
-                res = i + 1;
+            int leftLen = lenAtIndex[arr[i] - 1]; // Length of segment ending just before arr[i]
+            int rightLen = lenAtIndex[arr[i] + 1]; // Length of segment starting just after arr[i]
+
+            int newLen = leftLen + rightLen + 1; // New length of the segment that includes arr[i]
+
+            // Update the lengths at arr[i], arr[i] - leftLen, and arr[i] + rightLen
+            lenAtIndex[arr[i]] = newLen;
+            lenAtIndex[arr[i] - leftLen] = newLen;
+            lenAtIndex[arr[i] + rightLen] = newLen;
+
+            // Update countLen array to reflect the change in segment lengths
+            countLen[leftLen]--;
+            countLen[rightLen]--;
+            countLen[newLen]++;
+
+            // Check if there is any segment of length m after this step
+            if (countLen[m] > 0) {
+                ans = i + 1; // Update ans to the current step (1-based index)
+            }
         }
-        return res;
+
+        return ans;
     }
+}
