5f956b31db57e3289154ae63f67f2acf7d4b7d6a
Lucas Alcolea Oue <121897566+OueSan@users.noreply.github.com>
2025-12-23 12:30:00 +0900

    Documentation

--- Done/Binary_Search_307_Questions/107/Solution.java
+++ Done/Binary_Search_307_Questions/107/Solution.java
@@ -1,13 +1,50 @@
-public int minOperations(int[] nums, int x) {
-	int sum = 0;
-	for (int num: nums) sum += num;
+class Solution {
+    public int minOperations(int[] nums, int x) {
+        
+        /*equivalently u keep one middle subarray
+        let total=sum(nums);
+        so targetSum=total-x;
+        reframe:
+        find the longest subarray having sum=targetSum;
+        return n-longestLen;
+        */
 
-	int maxLength = -1, currSum = 0;
-	for (int l=0, r=0; r<nums.length; r++) {
-		currSum += nums[r];
-		while (l <= r && currSum > sum - x) currSum -= nums[l++];
-		if (currSum == sum - x) maxLength = Math.max(maxLength, r-l+1);
-	}
+        int n=nums.length;
 
-	return maxLength == -1 ? -1 : nums.length - maxLength;
+        long total=0L;
+        total=Arrays.stream(nums).asLongStream().sum();
+        //int total=Arrays.stream(nums).sum();
+        long target=total-x;
+        if(target==0)
+        {
+            return n;
+        }
+        if(target==-1)
+        {
+            return -1;//since 1 <= nums[i] <= 10^4
+
+        }
+
+        long curSum=0L;
+        int left=0;
+        int longest=-1;
+        for(int right=0;right<n;right++)
+        {
+            curSum+=nums[right];
+            while(left<=right && curSum>target)
+            {
+                
+                curSum-=nums[left];
+                left++;
+            }
+            if(curSum==target)
+            {
+                longest=Math.max(longest,right-left+1);
+            }
+
+        }
+        return longest==-1?-1:n-longest;
+
+
+    }
 }
