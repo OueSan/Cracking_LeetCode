8ad983eeb6437d0e60317b7a6855a44151b1dd34
Lucas Alcolea Oue <121897566+OueSan@users.noreply.github.com>
2025-12-31 11:29:35 +0900

    Documentation

--- Done/Binary_Search_307_Questions/113/Solution.py
+++ Done/Binary_Search_307_Questions/113/Solution.py
@@ -1,21 +1,8 @@
 class Solution:
-    def maxValue(self, events: List[List[int]], k: int) -> int:
-        n = len(events)
-        events.sort(key = lambda x: x[1])
-        ends = [e[1] for e in events]
-        dps = []
-        for i, (start, end, value) in enumerate(events):
-            last_attend_index = bisect.bisect_left(ends, start) - 1
-            new_dp = dps[i - 1][:] if i > 0 else [0] * k
-            if value > new_dp[0]:
-                    new_dp[0] = value
-            if last_attend_index >= 0:
-                dp = dps[last_attend_index]
-                for j in range(1, k):
-                    if dp[j - 1] == 0:
-                        break
-                    new_value = dp[j - 1] + value
-                    if new_value > new_dp[j]:
-                        new_dp[j] = new_value
-            dps.append(new_dp)
-        return max(dps[n - 1])
+    def minimumSize(self, nums: List[int], maxOps: int) -> int:
+        low, high = 1, max(nums) # min and max posible bags
+        while low < high:
+            mid = (low + high) // 2
+            if sum((n - 1) // mid for n in nums) <= maxOps: high = mid
+            else: low = mid + 1
+        return high
