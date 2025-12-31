78f50414bb349d6b664deb88935fdacc811a6b46
Lucas Alcolea Oue <121897566+OueSan@users.noreply.github.com>
2025-12-31 11:30:19 +0900

    Documentation

--- Done/Binary_Search_307_Questions/113/Solution.java
+++ Done/Binary_Search_307_Questions/113/Solution.java
@@ -1,14 +1,15 @@
 class Solution {
-    public int maxValue(int[][] events, int k) {
-        Arrays.sort(events, Comparator.comparingInt(a -> a[0]));
-        return dfs(events, 0, -1, k);
-    }
-
-    int dfs(int[][] ev, int i, int lastEnd, int k) {
-        if (i == ev.length || k == 0) return 0;
-        int take = 0;
-        if (ev[i][0] > lastEnd) take = ev[i][2] + dfs(ev, i + 1, ev[i][1], k - 1);
-        int skip = dfs(ev, i + 1, lastEnd, k);
-        return Math.max(take, skip);
+    public int minimumSize(int[] nums, int maxOps) {
+        int low = 1, high = Arrays.stream(nums).max().getAsInt();
+        while (low < high) {
+            int mid = (low + high) / 2;
+            int ops = 0;
+            for (int n : nums) {
+                ops += (n - 1) / mid;
+            }
+            if (ops <= maxOps) high = mid;
+            else low = mid + 1;
+        }
+        return high;
     }
 }
